1f9d5b81c8adff54612a4c6cadd2a23b9a67221f
mateo <mateotempestiruffino@gmail.com>
2023-06-01 18:55:24 -0300

    pypdf2TXT

--- pypdf2.py
+++ pypdf2.py
@@ -1,12 +1,17 @@
 import PyPDF2
 
 pdf = open("Velas.pdf", "rb")
+output_file = open("texto_extraido.txt", "w", encoding="utf-8")
 
 reader = PyPDF2.PdfReader(pdf)
 
 for no_page in range(len(reader.pages)):
-    print("Pagina ",no_page+1)
-    info_page =reader._get_page(no_page)
+    output_file.write("Pagina " + str(no_page+1) + "\n")
+    info_page = reader.pages[no_page]
     extract_info = info_page.extract_text()
-    print(extract_info)
-    print("******************")
+    output_file.write("******************\n")
+    output_file.write(extract_info + "\n")
+    output_file.write("******************\n")
+
+output_file.close()
+pdf.close()
